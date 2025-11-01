ef20365760c6f45124e8f4626752246553334350
Mani <pesarmani3@gmail.com>
2025-11-01 23:16:03 +0330

    safe login dar page asli

--- Js/web.js
+++ Js/web.js
@@ -1,13 +1,31 @@
-function setYear(){
+function setYear() {
   var y = document.getElementById("y");
-  if(y){
-    y.textContent = new Date().getFullYear();
-  }
+  if (y) y.textContent = new Date().getFullYear();
 }
 setYear();
 
-function goNext(){
+function goNext() {
   var dot = document.getElementById("dot");
-  if(dot){ dot.textContent = "..."; }
+  if (dot) dot.textContent = "...";
   location.hash = "#gallery";
 }
+
+window.addEventListener("DOMContentLoaded", function () {
+  const user = localStorage.getItem("username");
+  const signin = document.getElementById("signin");
+
+  if (user && signin) {
+    signin.outerHTML = `
+      <div id="user-info">
+        <span id="welcome">سلام، ${user} 👋</span>
+        <button id="logout">خروج</button>
+      </div>
+    `;
+
+    const logout = document.getElementById("logout");
+    logout.addEventListener("click", () => {
+      localStorage.removeItem("username"); // حذف نام کاربر
+      location.reload(); // رفرش صفحه برای بازگرداندن آیکون
+    });
+  }
+});
