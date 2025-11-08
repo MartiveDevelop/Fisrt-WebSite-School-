129b0d9389ee65051efe2bb1ef09a6e6821e60bf
Mani <pesarmani3@gmail.com>
2025-11-08 22:51:37 +0330

    make a real website with ndex name and things that iknow

--- Js/login.js
+++ Js/login.js
@@ -1,11 +1,12 @@
-function login(){
+function login() {
   var u = document.getElementById("u");
   var p = document.getElementById("p");
   var ok = document.getElementById("ok");
 
-  if(u && p && u.value !== "" && p.value !== ""){
-    window.location.href = "../html/website.html";
-  }else{
-    if(ok){ ok.textContent = "پر کن!"; }
+  if (u && p && u.value !== "" && p.value !== "") {
+    localStorage.setItem("username", u.value);
+    window.location.href = "index.html";
+  } else {
+    if (ok) ok.textContent = "پر کن!";
   }
 }
